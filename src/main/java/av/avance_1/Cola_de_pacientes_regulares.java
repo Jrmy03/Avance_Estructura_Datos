4af3e6399b420c7cda19fd61a16072ff9809b5a3
Jrmy03 <144741448+Jrmy03@users.noreply.github.com>
2024-10-18 19:39:49 -0600

    Salvando el proyecto

---
+++ src/main/java/av/avance_1/Cola_de_pacientes_regulares.java
@@ -0,0 +1,14 @@
+package av.avance_1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cola_de_pacientes_regulares {
+    
+}
